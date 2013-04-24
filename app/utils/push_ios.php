0f00e202680d0664cf64a6090b181a960fb7e5ee
earus <earus.mail@gmail.com>
2013-04-24 15:34:44 +0900

    apns 간단한 예외처리들 적용

--- app/utils/push_ios.php
+++ app/utils/push_ios.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__FILE__) . "/simple_apns_client.php";
+
 class IosPush {
 	const COMMAND_PUSH = 1; /* Payload command. */
 	const DEVICE_BINARY_SIZE = 32;
@@ -32,12 +34,9 @@ class IosPush {
 			$device_tokens[] = $device['device_token'];
 		}
 		
-		list($error_code, $error_string, $error_response_list) =
-					self::doSendPush($device_tokens, $message, $notification);
+		$error_response_list = self::doSendPush($device_tokens, $message, $notification);
 		
 		return array('sent' => sizeof($devices),
-					'error_code' => $error_code,
-					'error_string' => $error_string,
 					'error_response_list' => $error_response_list);
 	}
 	
@@ -58,82 +57,22 @@ class IosPush {
 		define('APNS_CERT_FILENAME', 'apns-distribution.pem');
 		define('APNS_CERT_PATH', dirname(__FILE__) . '/' . APNS_CERT_FILENAME);
 		
+		
+		$push = new SimpleApnsClient(SimpleApnsClient::ENVIRONMENT_PRODUCTION,
+								APNS_CERT_PATH);
+		
 	    $payload = self::getPayloadInJson($message, $notification);
+		$push->setPayload($payload);
 		
-		$stream_context = stream_context_create();
-		stream_context_set_option($stream_context, 'ssl', 'local_cert', APNS_CERT_PATH);
-		$apns = stream_socket_client('ssl://gateway.push.apple.com:2195',
-								$error, $error_string, 2, STREAM_CLIENT_CONNECT, $stream_context);
-		stream_set_blocking($apns, 0);	// fread가 바로 리턴하도록
-
-		$error_response_list = array();
-		
-		if($apns) {
-			foreach ($device_tokens as $device_token) {
-				$apns_message  = pack('CNNnH*', self::COMMAND_PUSH, 0, 0, self::DEVICE_BINARY_SIZE, $device_token);
-				$apns_message .= pack('n', strlen($payload));
-				$apns_message .= $payload;
-				
-				fwrite($apns, $apns_message);
-				
-				usleep(100 * 1000);
-				
-				$error_response = self::checkError($apns);
-				if ($error_response != null) {
-					array_push($error_response_list, array('device_token' => $device_token,
-														'error_response' => $error_response));
-				}
-			}
-
-			fclose($apns);
-		} else {
-			$error_string = "[storyholic] unable to connect to apn. " . $error_string;
+		foreach ($device_tokens as $device_token) {
+			$push->addDeviceToken($device_token);
 		}
 		
-		return array($error, $error_string, $error_response_list);
-	}
-	
-	// TODO: unchecked
-	private static function checkError($apns) {
-		/*
-		 * byte[0] = always 8
-		 * byte[1] = StatusCode
-		 * bytes[2~5] = identifier(rowID)
-		 * Should return nothing if OK.
-		 */
-		$apple_error_response = fread($apns, 6); 
-
-		if ($apple_error_response) {
-            $error_response = unpack('Ccommand/Cstatus_code/Nidentifier', $apple_error_response);
-
-            if ($error_response['status_code'] == '0') {
-                $error_response['status_description'] = '0-No errors encountered';
-            } else if ($error_response['status_code'] == '1') {
-                $error_response['status_description'] = '1-Processing error';
-            } else if ($error_response['status_code'] == '2') {
-                $error_response['status_description'] = '2-Missing device token';
-            } else if ($error_response['status_code'] == '3') {
-                $error_response['status_description'] = '3-Missing topic';
-            } else if ($error_response['status_code'] == '4') {
-                $error_response['status_description'] = '4-Missing payload';
-            } else if ($error_response['status_code'] == '5') {
-                $error_response['status_description'] = '5-Invalid token size';
-            } else if ($error_response['status_code'] == '6') {
-                $error_response['status_description'] = '6-Invalid topic size';
-            } else if ($error_response['status_code'] == '7') {
-                $error_response['status_description'] = '7-Invalid payload size';
-            } else if ($error_response['status_code'] == '8') {
-                $error_response['status_description'] = '8-Invalid token';
-            } else if ($error_response['status_code'] == '255') {
-                $error_response['status_description'] = '255-None (unknown)';
-            } else {
-                $error_response['status_description'] = $error_response['status_code'].'-Not listed';
-            }
-
-			return $error_response;
-       }
-
-       return null;
+		$push->connect();
+		$error_response_list = $push->send();
+		$push->disconnect();
+		
+		return $error_response_list;
 	}
 }
 
