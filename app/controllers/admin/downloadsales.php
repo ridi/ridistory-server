62ca2f9bb675a53ccf1a631bae777d6917b72b26
Daeyeol Ryu <dy@yoobato.com>
2014-02-12 14:39:08 +0900

    [charging] 매출확인, Admin에서 Model+Controller로 변경. // CMS 매출(책) 상세정보 추가.

---
+++ app/controllers/admin/downloadsales.php
@@ -0,0 +1,101 @@
+<?php
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminDownloadSalesControllerProvider implements ControllerProviderInterface
+{
+    public function connect(Application $app)
+    {
+        $admin = $app['controllers_factory'];
+
+        $admin->get('list', array($this, 'downloadSalesList'));
+        $admin->get('{b_id}', array($this, 'downloadSalesDetail'));
+
+        return $admin;
+    }
+
+    public function downloadSalesList(Request $req, Application $app)
+    {
+        $total_sales = 0;
+        $total_free_download = 0;
+        $total_charged_download = 0;
+
+        $download_sales = DownloadSales::getWholeList();
+
+        $today = strtotime("now");
+        foreach ($download_sales as &$ds)
+        {
+            if (strtotime($ds['begin_date']) <= $today && strtotime($ds['end_date']) >= $today && $ds['is_completed'] == false)
+            {
+                $status = "연재중";
+            }
+            else
+            {
+                switch ($ds['end_action_flag'])
+                {
+                    case 0:
+                        $status = "완결(모두 공개)";
+                        break;
+                    case 1:
+                        $status = "완결(모두 잠금)";
+                        break;
+                    case 2:
+                        $status = "게시종료(비공개)";
+                        break;
+                }
+            }
+            $ds['status'] = $status;
+
+            $ds['total_sales'] *= 100;  // 원(Won) 단위로 변환해서 표현현
+
+            // 헤더에 들어갈 정보 계산
+            $total_sales += $ds['total_sales'];
+            $total_free_download += $ds['free_download'];
+            $total_charged_download += $ds['charged_download'];
+        }
+
+        $header = array(
+            'total_sales' => $total_sales,
+            'total_free_download' => $total_free_download,
+            'total_charged_download' => $total_charged_download
+        );
+
+        return $app['twig']->render(
+            '/admin/download_sales_list.twig',
+            compact('header', 'download_sales'));
+    }
+
+    public function downloadSalesDetail(Request $req, Application $app, $b_id)
+    {
+        $total_sales = 0;
+        $total_free_download = 0;
+        $total_charged_download = 0;
+
+        $download_sales = DownloadSales::get($b_id);
+        $download_sales_detail = DownloadSales::getDetail($b_id);
+        foreach($download_sales_detail as &$dsd)
+        {
+            $dsd['total_coin_amount'] *= 100;
+
+            // 푸터에 들어갈 정보 계산
+            $total_sales += $dsd['total_coin_amount'];
+            $total_free_download += $dsd['total_free_download'];
+            $total_charged_download += $dsd['total_charged_download'];
+        }
+
+        $footer = array(
+            'total_sales' => $total_sales,
+            'total_free_download' => $total_free_download,
+            'total_charged_download' => $total_charged_download
+        );
+
+        return $app['twig']->render(
+            'admin/download_sales_detail.twig',
+            compact('download_sales', 'download_sales_detail', 'footer'));
+    }
+}
+
+?>
---
+++ app/models/DownloadSales.php
@@ -0,0 +1,40 @@
+<?php
+
+class DownloadSales
+{
+    public static function get($b_id)
+    {
+        $sql = <<<EOT
+select b.id b_id, b.title, b.author, b.begin_date, b.end_date, b.end_action_flag, sum(if(coin_amount=0, 1, 0)) free_download, sum(if(coin_amount!=0, 1, 0)) charged_download, sum(coin_amount) total_sales from purchase_history ph
+ left join (select id, b_id, price from part) p on p.id = ph.p_id
+ left join (select * from book) b on b.id = p.b_id
+where b_id = ?
+EOT;
+        global $app;
+        return $app['db']->fetchAssoc($sql, array($b_id));
+    }
+
+    public static function getWholeList()
+    {
+        $sql = <<<EOT
+select b.id b_id, b.title, b.author, b.begin_date, b.end_date, b.end_action_flag, sum(if(coin_amount=0, 1, 0)) free_download, sum(if(coin_amount!=0, 1, 0)) charged_download, sum(coin_amount) total_sales from purchase_history ph
+ left join (select id, b_id, price from part) p on p.id = ph.p_id
+ left join (select * from book) b on b.id = p.b_id
+group by b_id order by (count(*) * p.price) desc
+EOT;
+        global $app;
+        return $app['db']->fetchAll($sql);
+    }
+
+    public static function getDetail($b_id)
+    {
+        $sql = <<<EOT
+select p.id p_id, p.seq, p.title, ifnull(total_free_download, 0) total_free_download, ifnull(total_charged_download, 0) total_charged_download, ifnull(total_coin_amount, 0) total_coin_amount from part p
+ left join (select p_id, count(*) total_free_download from purchase_history where coin_amount = 0 group by p_id) ph on p.id = ph.p_id
+ left join (select p_id, count(*) total_charged_download, sum(coin_amount) total_coin_amount from purchase_history where coin_amount > 0 group by p_id) ph2 on p.id = ph2.p_id
+where b_id = ? order by p.seq
+EOT;
+        global $app;
+        return $app['db']->fetchAll($sql, array($b_id));
+    }
+}
