37b19f84475e9ecb2eafdf51d156b6e439ed7649
Daeyeol Ryu <dy@yoobato.com>
2014-04-29 14:32:06 +0900

    Public Key 수정.

--- src/Story/Model/CoinBilling.php
+++ src/Story/Model/CoinBilling.php
@@ -61,9 +61,9 @@ class CoinBilling
 
         //TODO: 추후에 Purchase Status API가 안정화되면, Purchase Status API로 교체
         // Signature 검증
-        $iab_public_key =  '-----BEGIN PUBLIC KEY-----\n'
-                         . chunk_split(self::IAB_PUBLIC_KEY, 64,'\n')
-                         . '-----END PUBLIC KEY-----';
+        $iab_public_key =  "-----BEGIN PUBLIC KEY-----\n"
+                         . chunk_split(self::IAB_PUBLIC_KEY, 64,"\n")
+                         . "-----END PUBLIC KEY-----";
         $iab_public_key = openssl_get_publickey($iab_public_key);
         $signature = base64_decode($signature);
         $is_valid_iab = openssl_verify($values['purchase_data'], $signature, $iab_public_key);
