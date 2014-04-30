bff830183a07e48b163581dc14e6254b23b17404
Daeyeol Ryu <dy@yoobato.com>
2014-04-30 12:00:55 +0900

    작가의 다른 작품 잘못된 쿼리 수정.

--- src/Story/Model/RecommendedBook.php
+++ src/Story/Model/RecommendedBook.php
@@ -33,7 +33,7 @@ class RecommendedBook
     public static function hasRecommendedBooks($b_id)
     {
         $sql = <<<EOT
-select ifnull(count(*), 0) from recommended_book where b_id = ? and store_id != '' and title != ''
+select count(*) from recommended_book where b_id = ? and store_id != '' and title != ''
 EOT;
         global $app;
         return $app['db']->fetchColumn($sql, array($b_id));
